3f50933eb7c29e0fd9643496cf5e830a0777e0a1
Andrey <161240497+itraffig@users.noreply.github.com>
2024-02-25 21:24:44 +0300

    исправление

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
 'use strict';
 
     const browse = new PopupMenu('browse', 'browse-popup-menu', 'drop-menu_display_flex');
-    const cart = new Cart('json/products.json');
+    const cart = new Cart('products.json');
